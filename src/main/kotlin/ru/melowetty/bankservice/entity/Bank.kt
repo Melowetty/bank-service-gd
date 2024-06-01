c0c93180418e467703ed831b9d6d45523f3f643b
Denis Malinin <melowetty@mail.ru>
2024-06-02 04:36:06 +0500

    remove unused fields from bank and add non null attr

--- src/main/kotlin/ru/melowetty/bankservice/entity/Bank.kt
+++ src/main/kotlin/ru/melowetty/bankservice/entity/Bank.kt
@@ -1,20 +1,13 @@
 package ru.melowetty.bankservice.entity
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.validation.constraints.Pattern
-import org.hibernate.validator.constraints.Length
-import ru.melowetty.bankservice.utils.Constants
+import jakarta.persistence.*
 
 @Entity
 data class Bank(
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     val id: Long,
-    
-    @Length(min = 9, max = 9, message = "Длина БИК должна быть равна 9 цифрам")
-    @Pattern(regexp = Constants.BIK_PATTERN, message = "БИК должен состоять только из цифр!")
+
+    @Column(nullable = false)
     val bic: String,
 )
