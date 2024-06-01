82143286a5cf2b66190f7abb363c9b9a64b54f0f
Denis Malinin <melowetty@mail.ru>
2024-06-02 03:51:59 +0500

    initial commit

---
+++ src/main/kotlin/ru/melowetty/bankservice/BankServiceApplication.kt
@@ -0,0 +1,11 @@
+package ru.melowetty.bankservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BankServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<BankServiceApplication>(*args)
+}
---
+++ src/test/kotlin/ru/melowetty/bankservice/BankServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.melowetty.bankservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BankServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
