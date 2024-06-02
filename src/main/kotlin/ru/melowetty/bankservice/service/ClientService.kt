c6855acc5c5d7dde6ff75a4a87467653251b7637
Denis Malinin <melowetty@mail.ru>
2024-06-02 20:30:53 +0500

    fix client service

--- src/main/kotlin/ru/melowetty/bankservice/service/ClientService.kt
+++ src/main/kotlin/ru/melowetty/bankservice/service/ClientService.kt
@@ -1,5 +1,6 @@
 package ru.melowetty.bankservice.service
 
+import jakarta.validation.Valid
 import ru.melowetty.bankservice.entity.Client
 import ru.melowetty.bankservice.model.CreateClientRequest
 import ru.melowetty.bankservice.model.EditClientRequest
@@ -8,8 +9,8 @@ interface ClientService {
     fun getClientById(id: Long): Client?
     fun deleteClientById(id: Long)
     fun patchClient(id: Long, fields: Map<String, Any?>): Client?
-    fun editClient(id: Long, editClientRequest: EditClientRequest): Client?
-    fun createClient(createClientRequest: CreateClientRequest): Client
+    fun editClient(id: Long, @Valid editClientRequest: EditClientRequest): Client?
+    fun createClient(@Valid createClientRequest: CreateClientRequest): Client
     fun getAllClients(): List<Client>
     fun getSortedClientsByField(field: String): List<Client>
 }
