f02e8ec4b8b4ba83266bdce6edfc9ea58acd193a
Denis Malinin <melowetty@mail.ru>
2024-06-02 15:27:00 +0500

    change signature of patch bank in bank service

--- src/main/kotlin/ru/melowetty/bankservice/service/BankService.kt
+++ src/main/kotlin/ru/melowetty/bankservice/service/BankService.kt
@@ -4,9 +4,9 @@ import ru.melowetty.bankservice.entity.Bank
 import ru.melowetty.bankservice.model.CreateBankRequest
 
 interface BankService {
-    fun getBankById(id: Long): Bank
+    fun getBankById(id: Long): Bank?
     fun deleteBankById(id: Long)
-    fun patchBank(id: Long, fields: Map<String, Any>): Bank
+    fun patchBank(id: Long, fields: Map<String, Any?>): Bank?
     fun createBank(createBankRequest: CreateBankRequest): Bank
     fun getAllBanks(): List<Bank>
     fun getSortedBanksByField(field: String): List<Bank>
