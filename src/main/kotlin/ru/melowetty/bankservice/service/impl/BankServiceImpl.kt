68bd062ccc768e6e6cd3bddde7a0d9fc0465a62a
Denis Malinin <melowetty@mail.ru>
2024-06-02 15:27:48 +0500

    add bank service impl and tests for service

---
+++ src/main/kotlin/ru/melowetty/bankservice/service/impl/BankServiceImpl.kt
@@ -0,0 +1,62 @@
+package ru.melowetty.bankservice.service.impl
+
+import jakarta.persistence.EntityNotFoundException
+import jakarta.validation.Valid
+import org.springframework.stereotype.Service
+import org.springframework.util.ReflectionUtils
+import org.springframework.validation.annotation.Validated
+import ru.melowetty.bankservice.entity.Bank
+import ru.melowetty.bankservice.model.CreateBankRequest
+import ru.melowetty.bankservice.repository.BankRepository
+import ru.melowetty.bankservice.service.BankService
+import ru.melowetty.bankservice.utils.ObjectUtils
+
+@Service
+@Validated
+class BankServiceImpl(
+    private val bankRepository: BankRepository,
+): BankService {
+    override fun getBankById(id: Long): Bank? {
+        return bankRepository.findById(id).orElse(null)
+    }
+
+    override fun deleteBankById(id: Long) {
+        val isExists = bankRepository.existsById(id)
+        if (!isExists) throw EntityNotFoundException("Банк с таким ID не найден!")
+        bankRepository.deleteById(id)
+    }
+
+    override fun patchBank(id: Long, fields: Map<String, Any?>): Bank? {
+        val bank = getBankById(id) ?: return null
+        fields.forEach { (t, u) ->
+            if(t != "id") {
+                val field = ReflectionUtils.findField(Bank::class.java, t)
+                if (field != null) {
+                    field.trySetAccessible()
+                    ReflectionUtils.setField(field, bank, u)
+                }
+            }
+        }
+        return bankRepository.save(bank)
+    }
+
+    override fun createBank(@Valid createBankRequest: CreateBankRequest): Bank {
+        val bank = Bank(
+            id = 0,
+            bic = createBankRequest.bic,
+            name = createBankRequest.name,
+            deposits = listOf()
+        )
+        return bankRepository.save(bank)
+    }
+
+    override fun getAllBanks(): List<Bank> {
+        return bankRepository.findAll().toList()
+    }
+
+    override fun getSortedBanksByField(field: String): List<Bank> {
+        return ObjectUtils.sortByField(Bank::class, field = field, data = getAllBanks())
+            ?: throw RuntimeException("Поле с таким названием не найдено!")
+    }
+
+}
---
+++ src/test/kotlin/ru/melowetty/bankservice/service/BankServiceTest.kt
@@ -0,0 +1,113 @@
+package ru.melowetty.bankservice.service
+
+import jakarta.persistence.EntityNotFoundException
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
+import ru.melowetty.bankservice.entity.Bank
+import ru.melowetty.bankservice.model.CreateBankRequest
+import ru.melowetty.bankservice.repository.BankRepository
+import ru.melowetty.bankservice.service.impl.BankServiceImpl
+import java.util.*
+
+@ExtendWith(MockitoExtension::class)
+class BankServiceTest {
+    @InjectMocks
+    private lateinit var bankService: BankServiceImpl
+    @Mock
+    private lateinit var bankRepository: BankRepository
+
+    @Test
+    fun `normal get all banks`() {
+        val expected = listOf(
+            Bank(id = 1, bic = "123456789", name = "Tinkoff", deposits = listOf()),
+            Bank(id = 2, bic = "234532546", name = "Sber", deposits = listOf())
+        )
+
+        Mockito.`when`(bankRepository.findAll()).thenReturn(
+            expected.stream().toList()
+        )
+
+        val actual = bankService.getAllBanks()
+
+        Assertions.assertEquals(expected, actual, "Списки со всеми банками не равны изначальным банкам")
+    }
+
+    @Test
+    fun `get sorted banks by bic`() {
+        val expected = listOf(
+            Bank(id = 2, bic = "123456789", name = "Sber", deposits = listOf()),
+            Bank(id = 1, bic = "234532546", name = "Tinkoff", deposits = listOf()),
+        )
+        Mockito.`when`(bankRepository.findAll()).thenReturn(
+            expected.stream().toList()
+        )
+
+        val actual = bankService.getSortedBanksByField(field = "bic")
+
+        Assertions.assertEquals(expected, actual, "Списки с отсортированными банками по полю bic не равны")
+    }
+
+    @Test
+    fun `get bank by id when it is exists`() {
+        val expected = Bank(id = 1, bic = "234532546", name = "Tinkoff", deposits = listOf())
+
+        Mockito.`when`(bankRepository.findById(1)).thenReturn(Optional.of(expected))
+
+        val actual = bankService.getBankById(1)
+
+        Assertions.assertEquals(expected, actual, "Банк который должен быть не найден")
+    }
+
+    @Test
+    fun `get bank by id when it is no exists`() {
+        Mockito.`when`(bankRepository.findById(Mockito.any())).thenReturn(Optional.empty())
+
+        val actual = bankService.getBankById(1)
+
+        Assertions.assertNull(actual, "Банк который не существует найден")
+    }
+
+    @Test
+    fun `delete bank by id, which exists`() {
+        Mockito.`when`(bankRepository.existsById(1)).thenReturn(true)
+        bankService.deleteBankById(1)
+        Mockito.verify(bankRepository, Mockito.times(1)).deleteById(1)
+    }
+
+    @Test
+    fun `delete bank by id, which not exists`() {
+        Mockito.`when`(bankRepository.existsById(1)).thenReturn(false)
+        assertThrows<EntityNotFoundException> {
+            bankService.deleteBankById(1)
+        }
+        Mockito.verify(bankRepository, Mockito.never()).deleteById(1)
+    }
+
+    @Test
+    fun `patch bank`() {
+        val before = Bank(id = 1, bic = "234532546", name = "Tinkoff", deposits = listOf())
+        val after = Bank(id = 1, bic = "234532546", name = "Sber", deposits = listOf())
+        Mockito.`when`(bankRepository.findById(1)).thenReturn(Optional.of(before))
+        Mockito.`when`(bankRepository.save(after)).thenReturn(after)
+        val actual = bankService.patchBank(1, mapOf("name" to "Sber"))
+        Assertions.assertEquals(actual, after, "Банк не изменился после запроса на частичное изменение")
+    }
+
+    @Test
+    fun `create bank`() {
+        val expected = Bank(id = 1, bic = "234532546", name = "Tinkoff", deposits = listOf())
+        val createBankRequest = CreateBankRequest(
+            bic = "234532546",
+            name = "Tinkoff"
+        )
+        Mockito.`when`(bankRepository.save(expected.copy(id = 0))).thenReturn(expected)
+        val actual = bankService.createBank(createBankRequest)
+        Assertions.assertEquals(expected, actual, "Банк не добавился после запроса на добавление")
+    }
+}
