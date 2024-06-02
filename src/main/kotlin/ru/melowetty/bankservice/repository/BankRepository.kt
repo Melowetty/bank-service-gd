73d8bca12cfd137c17bab28a1746c5be95e24866
Denis Malinin <melowetty@mail.ru>
2024-06-02 15:26:07 +0500

    add repository annotation for repositories

--- src/main/kotlin/ru/melowetty/bankservice/repository/BankRepository.kt
+++ src/main/kotlin/ru/melowetty/bankservice/repository/BankRepository.kt
@@ -1,6 +1,8 @@
 package ru.melowetty.bankservice.repository
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 import ru.melowetty.bankservice.entity.Bank
 
+@Repository
 interface BankRepository: CrudRepository<Bank, Long>
--- src/main/kotlin/ru/melowetty/bankservice/repository/ClientRepository.kt
+++ src/main/kotlin/ru/melowetty/bankservice/repository/ClientRepository.kt
@@ -1,6 +1,8 @@
 package ru.melowetty.bankservice.repository
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 import ru.melowetty.bankservice.entity.Client
 
+@Repository
 interface ClientRepository: CrudRepository<Client, Long>
