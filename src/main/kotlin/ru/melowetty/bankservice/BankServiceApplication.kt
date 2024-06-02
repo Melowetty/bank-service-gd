8f15d98d5b30350a53741c88d6d3d8a65000afae
Denis Malinin <melowetty@mail.ru>
2024-06-02 23:44:25 +0500

    add out dating deposit checking every night scheduler

--- src/main/kotlin/ru/melowetty/bankservice/BankServiceApplication.kt
+++ src/main/kotlin/ru/melowetty/bankservice/BankServiceApplication.kt
@@ -2,8 +2,10 @@ package ru.melowetty.bankservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class BankServiceApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/ru/melowetty/bankservice/scheduler/DepositOutDatingCheckingWorker.kt
+++ src/main/kotlin/ru/melowetty/bankservice/scheduler/DepositOutDatingCheckingWorker.kt
@@ -1,9 +1,28 @@
 package ru.melowetty.bankservice.scheduler
 
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+import ru.melowetty.bankservice.repository.DepositRepository
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
 
 @Component
 class DepositOutDatingCheckingWorker(
+    private val depositRepository: DepositRepository,
 ) {
-
+    @Transactional
+    @Scheduled(cron = "0 0 0 * * ?")
+    fun outDatingDepositChecking() {
+        val currentDate = LocalDateTime.now()
+        val deposits = depositRepository.getDepositByIsOutDated()
+        deposits.map {
+            val difference = ChronoUnit.MONTHS.between(currentDate, it.dateOfOpen)
+            it.copy(isOutDated = difference > it.duration)
+        }.filter {
+            it.isOutDated
+        }.forEach {
+            depositRepository.save(it)
+        }
+    }
 }
